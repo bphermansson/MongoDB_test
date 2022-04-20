eb00fc7a06c7ea41a615c3b51dbabe3dc38420b4
Patrik Hermansson <hermansson.patrik@gmail.com>
2022-04-20 22:26:49 +0200

    Beginning with new struct

--- src/main.c
+++ src/main.c
@@ -36,6 +36,14 @@
 #define MAX_DBLIST_LENGTH 10
 #define BUFFSIZE 100
 
+typedef struct 
+{
+   uint8_t key;
+   uint8_t data;
+} struct_data;
+
+static struct_data data_struct[BUFFSIZE];
+
 //void create_new_doc(mongoc_collection_t *collection);
 void print_count (mongoc_collection_t *collection, bson_t *filter);
 //void list_posts(mongoc_collection_t *collection, char **posts_array);
@@ -74,17 +82,25 @@ int main()
    else 
    {
       printf("List current posts\n");
+      /*
       char *posts_array;
       posts_array = NULL;
       int length = 100;
       posts_array = malloc(length * sizeof(char));
 
       uint8_t no_of_posts = list_posts(collection, &posts_array);
+      */
+      data_struct[3].data = 1;
+      struct_data dbdata[BUFFSIZE];
+      struct_data *ptr_dbdata = dbdata;
+
+      list_posts(collection, ptr_dbdata);
+
       printf("Done listing\n");
 
       //for (int i = 0 ; i < sizeof(posts_array) ; i++)
       for (int i = 0 ; i < 3 ; i++)
-         printf("%d - %d ", i, posts_array[i]);
+         //printf("%d - %d ", i, posts_array[i]);
       printf("---\n");
    }
 
@@ -131,6 +147,7 @@ int list_posts(mongoc_collection_t *collection, char **posts_array)
 int *b = (int*)malloc(sizeof(int)*4);
 
    b[2] = 6;
+   b[0] = 6;
 
    int c=0;
    while (mongoc_cursor_next (cursor, &doc)) {
