d85ca1d1113e29b3f36c052fc3a9dcf3aace764c
Patrik Hermansson <hermansson.patrik@gmail.com>
2022-04-20 22:12:28 +0200

    Adjust gitignore

--- lib/ping/ping.c
+++ lib/ping/ping.c
@@ -1,4 +1,17 @@
 
+typedef struct 
+{
+    uint8_t state;
+    uint8_t pin_number;
+    uint8_t nr_of_con_reads;
+    bool initialized;
+    uint8_t level;
+    uint8_t prev_level;
+    uint8_t edge;
+} btn_struct;
+static btn_struct debouncer_btns[MAX_NO_OF_BTNS];
+
+
 int ping()
 {
     mongoc_database_t *database;
